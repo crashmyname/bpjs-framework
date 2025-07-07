c88fceed671cb0a7baeb1f03a16e6b8ce8d3a530
Fadli Azka Prayogi <fadliazkaprayogi1@gmail.com>
2025-07-07 13:16:06 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,3 @@
 use Helpers\Api;
 use Helpers\Response;
 
-Api::get('/', function () {
-    return Response::json(['message' => 'Dengan slash'], 200);
-});
-
-Api::get('/test', function () {
-    return Response::json(['message' => 'Ini /test'], 200);
-});
